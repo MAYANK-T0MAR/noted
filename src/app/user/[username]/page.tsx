fb5ded20afa2a241b02391dbef87b9f86402d796
Mayank Tomar <mayanktomar281@gmail.com>
2025-06-24 16:11:25 +0530

    fixed userPageProps params type

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,23 +1,21 @@
 import { Metadata } from "next";
 
 type userPageProps = {
-    params: {
-        username: string | Promise<string>
-    };
-}
+    params: Promise<{username: string;}>;
+};
 
 export async function generateMetadata({ params }: userPageProps): Promise<Metadata> {
-    const username = await params.username //to do : this is giving warning to use await even after using await
+    const username = (await params).username;
     return {
         title: username,
     };
 }
 
-export default async function userProfile({ params } : userPageProps) {
-    const username = await params.username
+export default async function userProfile({ params }: userPageProps) {
+    const username = (await params).username;
     return (
         <div>
-            {`this is  ${username}'s profile`}
+            {`This is ${username}'s profile`}
         </div>
-    )
+    );
 }
